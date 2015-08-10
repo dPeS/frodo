69202fa2e5efe3c0188acd2c24a484e21ecfef8f
Daniel Smoczyk <dsmoczyk@egnyte.com>
2015-08-10 13:46:28 +0200

    better for debugging

--- frodo/utils.py
+++ frodo/utils.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 
+
 def get_connection_from_env():
     """
     returns psycopg2 connection based on system enviroments settings
@@ -9,12 +10,10 @@ def get_connection_from_env():
     """
     first try to get UNIX socket for current shell user
     """
-    try:
-        user = os.environ['USER']
-        dbname = os.environ['USER']
-        connection = psycopg2.connect(dbname=dbname, user=user)
-        return connection
-    except:
-        pass
-
-    raise Exception('Could not find any route to database, pls check: ')
+    connection = psycopg2.connect(
+        dbname=os.environ['USER'],
+        user=os.environ['USER'],
+        port=5432,
+        host='/var/run/postgresql'
+    )
+    return connection
