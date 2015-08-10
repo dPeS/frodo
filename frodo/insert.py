b64d0fe3b6df88f25e5242b2da49187b655b461a
Daniel Smoczyk <dsmoczyk@egnyte.com>
2015-08-10 17:11:21 +0200

    add order_timeout

--- frodo/insert.py
+++ frodo/insert.py
@@ -1,4 +1,7 @@
+import datetime
+
 from frodo.constants import FRODO_MODULE_DATABASE_PREFIX as PREFIX
+from frodo.constants import DEFAULT_ORDER_TIMEOUT as TIMEOUT
 import psycopg2.extras
 
 
@@ -11,9 +14,11 @@ class InsertMixin:
         creates new cointainer
         """
         cursor = self.current_cursor()
-        cursor.execute("insert into {}_ea_container "
-                       "values(default)"
-                       "returning id".format(PREFIX))
+        cursor.execute(
+            "insert into {}_ea_container "
+            "values(default)"
+            "returning id".format(PREFIX)
+        )
         return cursor.fetchone()
 
     def _sql_insert_type(self, container_id, type_code, max_quantity):
@@ -21,10 +26,12 @@ class InsertMixin:
         creates new type for container
         """
         cursor = self.current_cursor()
-        cursor.execute(("insert into {}_ea_type("
-                        "container_id, code, max_quantity) "
-                        "values(%s, %s, %s)").format(PREFIX),
-                       (container_id, type_code, max_quantity))
+        cursor.execute(
+            "insert into {}_ea_type("
+            "container_id, code, max_quantity) "
+            "values(%s, %s, %s)".format(PREFIX),
+            (container_id, type_code, max_quantity)
+        )
 
     def _sql_insert_order(self, container_id, order_list):
         """
@@ -33,8 +40,17 @@ class InsertMixin:
         cursor = self.current_cursor()
         for type_code, quantity, begin, end in order_list:
             for cycle in xrange(quantity):
-                duration = psycopg2.extras.DateTimeTZRange(begin, end, '[]')
-                cursor.execute(("insert into {}_ea_order("
-                                "container_id, type_code, order_duration) "
-                                "values(%s, %s, %s)").format(PREFIX),
-                               (container_id, type_code, duration))
+                cursor.execute(
+                    "insert into {}_ea_order("
+                    "container_id, "
+                    "type_code, "
+                    "order_duration, "
+                    "order_timeout) "
+                    "values(%s, %s, %s, now() + %s)".format(PREFIX),
+                    (
+                        container_id,
+                        type_code,
+                        psycopg2.extras.DateTimeTZRange(begin, end, '[]'),
+                        datetime.timedelta(seconds=TIMEOUT),
+                    )
+                )
--- frodo/setup.py
+++ frodo/setup.py
@@ -12,40 +12,59 @@ class InitDatabaseMixin:
         cursor = self.current_cursor()
 
         # CONTAINER
-        cursor.execute(("create table "
-                        "{}_ea_container"
-                        "(id serial primary key)").format(PREFIX))
+        cursor.execute(
+            "create table "
+            "{}_ea_container"
+            "(id serial primary key)"
+            "".format(PREFIX)
+        )
         # ORDER TYPE
-        cursor.execute(("create table {prefix}_ea_type(code int, "
-                        "container_id int references "
-                        "{prefix}_ea_container(id), "
-                        "max_quantity int, "
-                        "primary key"
-                        "(code, container_id) )").format(prefix=PREFIX))
+        cursor.execute(
+            "create table {prefix}_ea_type("
+            "code int,"
+            "container_id int references {prefix}_ea_container(id),"
+            "max_quantity int,"
+            "primary key (code, container_id) "
+            ")".format(prefix=PREFIX)
+        )
         # ORDER
-        cursor.execute(("create table {prefix}_ea_order(id serial primary key, "
-                        "order_duration tstzrange, "
-                        "container_id int, "
-                        "type_code int, "
-                        "foreign key(container_id, type_code) "
-                        "references {prefix}_ea_type(container_id, code))"
-                        "").format(prefix=PREFIX))
+        cursor.execute(
+            "create table {prefix}_ea_order("
+            "id serial primary key,"
+            "order_duration tstzrange,"
+            "order_timeout timestamp,"
+            "container_id int,"
+            "type_code int,"
+            "foreign key(container_id, type_code) "
+            "references {prefix}_ea_type(container_id, code)"
+            ")".format(prefix=PREFIX)
+        )
 
         # TOOLS
         # internal FUNCTION used in constraint
-        cursor.execute(("create or replace function "
-                        "{prefix}_ea_check_orders_fn(tstzrange, int, int) "
-                        "returns bigint as "
-                        "$$ "
-                        "select (select max_quantity from "
-                        "{prefix}_ea_type where "
-                        "container_id = $2 and code = $3) "
-                        " - count(1) from {prefix}_ea_order where "
-                        "not isempty(order_duration * $1) "
-                        "and container_id = $2 and type_code = $3"
-                        "$$ language sql immutable;").format(prefix=PREFIX))
+        cursor.execute(
+            "create or replace function "
+            "{prefix}_ea_check_orders_fn(tstzrange, int, int) "
+            "returns bigint as "
+            "$$ "
+            "select (select max_quantity from {prefix}_ea_type "
+            "where container_id = $2 and code = $3)"
+            " - "
+            "count(1) from {prefix}_ea_order where "
+            "not isempty(order_duration * $1) "
+            "and container_id = $2 "
+            "and type_code = $3 "
+            "and order_timeout >= now()"
+            "$$ language sql immutable;"
+            "".format(prefix=PREFIX)
+        )
 
         # addint constraint (whole magic) to ORDER table
-        cursor.execute(("alter table {prefix}_ea_order add CHECK "
-                        "({prefix}_ea_check_orders_fn(order_duration, "
-                        "container_id, type_code) > 0)").format(prefix=PREFIX))
+        cursor.execute(
+            "alter table {prefix}_ea_order add CHECK "
+            "({prefix}_ea_check_orders_fn("
+            "order_duration,"
+            "container_id,"
+            "type_code) > 0)"
+            "".format(prefix=PREFIX)
+        )
