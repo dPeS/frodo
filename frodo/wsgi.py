930aa47c5333af1d5f83042e63597402854d6caf
Daniel Smoczyk <dsmoczyk@egnyte.com>
2015-08-13 10:20:10 +0200

    routing added + code organized

--- frodo/wsgi.py
+++ frodo/wsgi.py
@@ -1,3 +1,29 @@
+import json
+import frodo
+
+db = frodo.plain()
+
+
+def split_path(path):
+    return path.strip('/').lower().split('/', 5)
+
+
+def request_handler(method, route):
+    return (
+        '200 OK',
+        [('Content-Type', 'text/html')],
+        '',
+    )
+
+
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello Frodo ;)"]
+    route = split_path(env['PATH_INFO'])
+    method = env['REQUEST_METHOD']
+
+    code, response_headers, body = request_handler(method, route)
+
+    start_response(
+        code,
+        response_headers
+    )
+    return [json.dumps(body)]
