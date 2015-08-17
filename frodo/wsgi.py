8516c9496f005e93ab71ed8f2b280fe1a99609ad
Daniel Smoczyk <daniel.smoczyk@gmail.com>
2015-08-17 17:51:59 +0200

    route handler

--- frodo/wsgi.py
+++ frodo/wsgi.py
@@ -4,17 +4,33 @@ import frodo
 db = frodo.plain()
 
 
+class ObjID(object):
+    def __eq__(self, other):
+        return True
+
+
+ROUTES = (
+    (
+        ('POST', ['containers', ObjID()]),
+        lambda route, payload: {}
+    )
+)
+
+
 def split_path(path):
     return path.strip('/').lower().split('/', 5)
 
 
 def request_handler(method, route, env):
-    #payload = json.loads(env['wsgi.input'].getvalue())
+    for r, f in ROUTES:
+        m, p = r
+        if m == method and all([a == b for a, b in zip(p, route)]):
+            body = f(route, env['wsgi.input'])
 
     return (
         '200 OK',
         [('Content-Type', 'text/html')],
-        '',
+        body,
     )
 
 
